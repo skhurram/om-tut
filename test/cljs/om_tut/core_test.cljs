da57c844251bbfdf03bc504380c045962b971a97
skhurram <khurram@syedkhurram.com>
2014-06-11 23:16:43 +0200

    remove undefined function

--- test/cljs/om_tut/core_test.cljs
+++ test/cljs/om_tut/core_test.cljs
@@ -3,12 +3,16 @@
 (ns om-tut.core-test
   (:require-macros [cemerick.cljs.test :as m :refer (deftest testing are)])
   (:require [cemerick.cljs.test :as t]
-            [om-tut.core :refer (foo)]))
+            ))
+;;[om-tut.core :refer (foo)]
 
 (deftest foo-test
   (testing "I don't do a lot\n"
     (testing "Edge cases\n"
       (testing "(foo str)"
         (are [expected actual] (= expected actual)
+             "ClojureScript!"
+             "Hello, ClojureScript!" )
+        #_(are [expected actual] (= expected actual)
              "ClojureScript!" (foo "")
              "Hello, ClojureScript!" (foo nil))))))
